b57c239c36decfd4fd7feb1742ef113052f326ea
alaa-abuabid <alaa.abuabid@gmail.com>
2025-02-22 01:16:56 +0200

    move history stacks to datastore

--- task2/main.py
+++ task2/main.py
@@ -1,61 +1,63 @@
 from flask import Flask, request
 from google.cloud import datastore
+from datetime import datetime
 
 app = Flask(__name__)
 client = datastore.Client()
 
 ERROR_RESPONSE = "Invalid Parameter"
-DATASTORE_KIND_NAME = "task2"
-
-# Stacks to keep track of history and undone commands
-command_history_stack = []
-undo_history_stack = []
+MAIN_DATA_KIND = "task2"
+COMMAND_HISTORY_KIND = "CommandHistory"
+UNDO_HISTORY_KIND = "UndoHistory"
 
 def is_valid_parameter(param):
     return isinstance(param, str) and param.strip() and " " not in param
 
 def set_data(name, value):
-    key = client.key(DATASTORE_KIND_NAME, name)
+    key = client.key(MAIN_DATA_KIND, name)
     entity = datastore.Entity(key=key)
     entity.update({'value': value})
     client.put(entity)
 
 def get_data(name):
-    key = client.key(DATASTORE_KIND_NAME, name)
+    key = client.key(MAIN_DATA_KIND, name)
     return client.get(key)
 
 def delete_data(name):
-    key = client.key(DATASTORE_KIND_NAME, name)
+    key = client.key(MAIN_DATA_KIND, name)
     client.delete(key)
 
+def append_command(command, name, value, kind):
+    key = client.key(kind)
+    entity = datastore.Entity(key=key)
+    entity.update({
+        'command': command,
+        'name': name,
+        'value': value,
+        'timestamp': datetime.utcnow()
+    })
+    client.put(entity)
 
-def handle_undo_redo(source_stack, destination_stack):
-    if not source_stack:
-        return "NO COMMANDS"
-
-    command, name, value = source_stack.pop() # get and remove from source stack
-    destination_stack.append(command, name, value) # add the command to the destination stack
-    if command == 'SET':
-        if value is None:
-            delete_data(name)
-            return f"{name} = None"
-        else:
-            set_data(name, value)
-            return f"{name} = {value}"
-    elif command == 'UNSET':
-        set_data(name, value)
-        return f"{name} = {value}"
+def pop_command(kind):
+    query = client.query(kind=kind)
+    query.order = ['-timestamp']
+    results = list(query.fetch(limit=1))
+    if results:
+        entity = results[0]
+        client.delete(entity.key)
+        return entity['command'], entity['name'], entity['value']
+    return None, None, None
 
 @app.route('/set')
 def set_variable():
     name = request.args.get('name')
     value = request.args.get('value')
     if is_valid_parameter(name) and is_valid_parameter(value):
-        previous_entity = get_data(name) # we need to get old entity in order to add it to the history stack
+        previous_entity = get_data(name)
         if previous_entity:
-            command_history_stack.append(('SET', name, previous_entity['value'])) # set old entity to the command stack before overriding it
+            append_command('SET', name, previous_entity['value'], COMMAND_HISTORY_KIND)
         else:
-            command_history_stack.append(('SET', name, None)) # set old entity to the command stack before overriding it
+            append_command('SET', name, None, COMMAND_HISTORY_KIND)
         set_data(name, value)
         return f'{name} = {value}'
     else:
@@ -74,11 +76,11 @@ def get_variable():
 def unset_variable():
     name = request.args.get('name')
     if is_valid_parameter(name):
-        entity = get_data(name) # we need to get old entity in order to add it to the history stack
+        entity = get_data(name)
         if entity:
-            command_history_stack.append(('UNSET', name, entity['value']))  # set old entity to the command stack before removing it
+            append_command('UNSET', name, entity['value'], COMMAND_HISTORY_KIND)
             delete_data(name)
-            return entity['value'] if entity else 'None'
+            return f'{name} = None'
     else:
         return ERROR_RESPONSE
 
@@ -86,7 +88,7 @@ def unset_variable():
 def numequalto():
     value = request.args.get('value')
     if is_valid_parameter(value):
-        query = client.query(kind=DATASTORE_KIND_NAME)
+        query = client.query(kind=MAIN_DATA_KIND)
         query.add_filter('value', '=', value)
         results = list(query.fetch())
         return str(len(results))
@@ -95,19 +97,51 @@ def numequalto():
 
 @app.route('/undo')
 def undo():
-    handle_undo_redo(command_history_stack, undo_history_stack)
+    command, name, value = pop_command(COMMAND_HISTORY_KIND)
+    if command:
+        previous_entity = get_data(name)
+        if command == 'SET':
+            if value is None:
+                delete_data(name)
+                append_command('UNSET', name, previous_entity['value'], UNDO_HISTORY_KIND)# here!!!!
+                return f'{name} = None'
+            else:
+                set_data(name, value)
+                append_command('SET', name, previous_entity['value'], UNDO_HISTORY_KIND)
+                return f'{name} = {value}'
+        elif command == 'UNSET':
+            set_data(name, value)
+            append_command('SET', name, None, UNDO_HISTORY_KIND)
+            return f'{name} = {value}'
+    else:
+        return "NO COMMANDS"
 
 @app.route('/redo')
 def redo():
-    handle_undo_redo(undo_history_stack, command_history_stack)
+    command, name, value = pop_command(UNDO_HISTORY_KIND)
+    if command:
+        if command == 'SET':
+            set_data(name, value)
+            append_command('SET', name, value, COMMAND_HISTORY_KIND)
+            return f'{name} = {value}'
+        elif command == 'UNSET':
+            delete_data(name)
+            append_command('UNSET', name, None, COMMAND_HISTORY_KIND)
+            return f'{name} = None'
+    else:
+        return "NO COMMANDS"
 
 @app.route('/end')
 def end():
-    query = client.query(kind=DATASTORE_KIND_NAME)
+    query = client.query(kind=MAIN_DATA_KIND)
     keys = [entity.key for entity in query.fetch()]
     client.delete_multi(keys)
-    command_history_stack.clear()
-    undo_history_stack.clear()
+    query = client.query(kind=COMMAND_HISTORY_KIND)
+    keys = [entity.key for entity in query.fetch()]
+    client.delete_multi(keys)
+    query = client.query(kind=UNDO_HISTORY_KIND)
+    keys = [entity.key for entity in query.fetch()]
+    client.delete_multi(keys)
     return "CLEANED"
 
 if __name__ == '__main__':
